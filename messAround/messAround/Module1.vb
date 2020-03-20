2adf26aab4e96c6db1c305b47e21276ea9a06c9f
RownakCodes <47391823+RownakCodes@users.noreply.github.com>
2020-03-20 23:03:33 +0600

    Update Module1.vb

--- messAround/messAround/Module1.vb
+++ messAround/messAround/Module1.vb
@@ -1,108 +1,21 @@
-﻿Module Module1
+Imports System
 
-    Sub Main()
-        Console.WriteLine("                                        Denery to 8 bit Binary:")
+Module Program
+    Sub Main(args As String())
+        Dim str, newstr As String
+        str = ""
+        Dim bin As Integer = 56
 
-        Dim input, entered As Integer
-        Dim conversion As String
+        While bin > 0
+            str += CStr(bin Mod 2)
+            bin = bin \ 2
+        End While
 
-        Console.WriteLine("Enter the number :")
-        input = Console.ReadLine()
-
-        conversion = ""
-        entered = input
-
-
-        For x = 0 To 7
-
-            If input > 255 Then
-                Console.WriteLine("The number you entered is too big for a 8 bit binary to store!")
-                Exit For
-            End If
-
-
-            Select Case x
-                Case 0
-                    If input >= 128 Then
-                        input = input - 128
-                        conversion = conversion + "1"
-                    Else
-                        conversion = conversion + "0"
-                    End If
-
-                Case 1
-
-                    If input >= 64 Then
-                        input = input - 64
-                        conversion = conversion + "1"
-                    Else
-                        conversion = conversion + "0"
-                    End If
-
-                Case 2
-
-                    If input >= 32 Then
-                        input = input - 32
-                        conversion = conversion + "1"
-                    Else
-                        conversion = conversion + "0"
-                    End If
-
-
-                Case 3
-
-                    If input >= 16 Then
-                        input = input - 16
-                        conversion = conversion + "1"
-                    Else
-                        conversion = conversion + "0"
-                    End If
-
-
-                Case 4
-
-                    If input >= 8 Then
-                        input = input - 8
-                        conversion = conversion + "1"
-                    Else
-                        conversion = conversion + "0"
-                    End If
-
-
-                Case 5
-
-                    If input >= 4 Then
-                        input = input - 4
-                        conversion = conversion + "1"
-                    Else
-                        conversion = conversion + "0"
-                    End If
-
-                Case 6
-
-                    If input >= 2 Then
-                        input = input - 2
-                        conversion = conversion + "1"
-                    Else
-                        conversion = conversion + "0"
-                    End If
-
-
-                Case 7
-
-                    If input >= 1 Then
-                        input = input - 1
-                        conversion = conversion + "1"
-                    Else
-                        conversion = conversion + "0"
-                    End If
-
-            End Select
-
-
+        'Code for reversing a string
+        newstr = ""
+        For index = Len(str) - 1 To 0 Step -1
+            newstr = newstr + str(index)
         Next
-
-        Console.WriteLine("The binary of " & entered & " is : " & conversion)
+        Console.WriteLine(newstr)
     End Sub
-
 End Module
